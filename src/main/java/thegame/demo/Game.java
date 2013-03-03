e10ff6bd0755d9c19a53f405fafb608dc32558f2
Alexandre Gauthier <alex@underwares.org>
2013-03-02 19:34:24 -0500

    Cleanup GL imports

--- src/main/java/thegame/demo/Game.java
+++ src/main/java/thegame/demo/Game.java
@@ -1,16 +1,6 @@
 package thegame.demo;
 
-import static org.lwjgl.opengl.GL11.GL_BLEND;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
-import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
-import static org.lwjgl.opengl.GL11.glBlendFunc;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glDisable;
-import static org.lwjgl.opengl.GL11.glEnable;
-import static org.lwjgl.opengl.GL11.glViewport;
+import org.lwjgl.opengl.*;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
@@ -80,10 +70,14 @@ public class Game {
 	 * Initialize OpenGL Context
 	 */
 	private void glInit() {
-		glDisable(GL_DEPTH_TEST);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Black
+		//GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		//GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Black
+		
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
 	}
 	
 	/*
@@ -91,7 +85,7 @@ public class Game {
 	 */
 	private void render() {
 		// Clear screen
-		glClear(GL_COLOR_BUFFER_BIT);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 		
 		// TODO: stub - render game
 	}
@@ -100,7 +94,7 @@ public class Game {
 	 * Resize render surface
 	 */
 	private void resize() {
-		glViewport(0, 0, Display.getWidth(), Display.getHeight());
+		GL11.glViewport(0, 0, Display.getWidth(), Display.getHeight());
 		// TODO: stub - update matrices
 		
 	}
